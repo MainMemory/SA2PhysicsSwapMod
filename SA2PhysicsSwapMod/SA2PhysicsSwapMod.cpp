7d3923f81e6fc1ebe4fed36a6c6ae8ee562732b7
MainMemory <mainmemory@yahoo.com>
2017-01-24 15:56:44 -0600

    Initial Release.

--- SA2PhysicsSwapMod/SA2PhysicsSwapMod.cpp
+++ SA2PhysicsSwapMod/SA2PhysicsSwapMod.cpp
@@ -2,5 +2,91 @@
 //
 
 #include "stdafx.h"
+#include "resource.h"
+#include "SA2ModLoader.h"
+#include "IniFile.hpp"
+#include <algorithm>
+using std::unordered_map;
+using std::string;
 
+HMODULE moduleHandle;
 
+static const string charnames[] = {
+	"sonic",
+	"shadow",
+	"tails",
+	"eggman",
+	"knuckles",
+	"rouge",
+	"mechtails",
+	"mecheggman",
+	"amy",
+	"supersonic",
+	"supershadow",
+	"unused",
+	"metalsonic",
+	"chaowalker",
+	"darkchaowalker",
+	"tikal",
+	"chaos",
+	"unused2",
+	"unused3",
+	"sadxsonic",
+	"sadxeggman",
+	"sadxtails",
+	"sadxknuckles",
+	"sadxtikal",
+	"sadxamy",
+	"sadxgamma",
+	"sadxbig",
+	"sadxsupersonic"
+};
+
+static unordered_map<string, uint8_t> charnamemap;
+
+static string trim(const string &s)
+{
+	auto st = s.find_first_not_of(' ');
+	if (st == string::npos)
+		st = 0;
+	auto ed = s.find_last_not_of(' ');
+	if (ed == string::npos)
+		ed = s.size() - 1;
+	return s.substr(st, (ed + 1) - st);
+}
+
+static uint8_t ParseCharacterID(const string &str, Characters def)
+{
+	string s = trim(str);
+	transform(s.begin(), s.end(), s.begin(), ::tolower);
+	auto ch = charnamemap.find(s);
+	if (ch != charnamemap.end())
+		return ch->second;
+	return def;
+}
+
+static const string keynames[] = { "Sonic", "Shadow", "Tails", "Eggman", "Knuckles", "Rouge", "MechTails", "MechEggman", "Amy", "SuperSonic", "SuperShadow", "Unused", "MetalSonic", "ChaoWalker", "DarkChaoWalker", "Tikal", "Chaos" };
+
+static PhysicsData *const PhysicsArray = (PhysicsData *)0x17391E8;
+
+extern "C"
+{
+	__declspec(dllexport) void __cdecl Init(const char *path, const HelperFunctions &helperFunctions)
+	{
+		for (uint8_t i = 0; i < LengthOfArray(charnames); i++)
+			charnamemap[charnames[i]] = i;
+		HRSRC hres = FindResource(moduleHandle, MAKEINTRESOURCE(IDR_MISC1), L"MISC");
+		const PhysicsData *tmp = (PhysicsData*)LockResource(LoadResource(moduleHandle, hres));
+		const IniFile *settings = new IniFile(std::string(path) + "\\mod.ini");
+		for (uint8_t i = 0; i < LengthOfArray(keynames); i++)
+		{
+			uint8_t c = ParseCharacterID(settings->getString("", keynames[i] + "Physics"), (Characters)i);
+			if (i == c) continue;
+			memcpy(&PhysicsArray[i], &tmp[c], offsetof(PhysicsData, anonymous_27));
+			PhysicsArray[i].Gravity = tmp[c].Gravity;
+		}
+		delete settings;
+	}
+
+	__declspec(dllexport) ModInfo SA2ModInfo = { ModLoaderVer };
+}
--- SA2PhysicsSwapMod/dllmain.cpp
+++ SA2PhysicsSwapMod/dllmain.cpp
@@ -1,14 +1,17 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "stdafx.h"
+extern HMODULE moduleHandle;
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-					 )
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
 {
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
+		moduleHandle = hModule;
+		break;
 	case DLL_THREAD_ATTACH:
 	case DLL_THREAD_DETACH:
 	case DLL_PROCESS_DETACH:
@@ -16,4 +19,3 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 	}
 	return TRUE;
 }
-
